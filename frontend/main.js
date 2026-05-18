8a350eee8e450dd6d9c0758ba2d959615648ae28
diditata <diana.chuiashova.ri.2025@lpnu.ua>
2026-05-18 17:45:04 +0300

    Add files via upload

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+document.getElementById("testBtn").addEventListener("click", function () {
+    window.location.href = "test.html"; // інша сторінка
+});
+
+document.getElementById("golovna").addEventListener("click", function () {
+    window.location.href = "main.html"; // інша сторінка
+});
+
+document.getElementById("uvijty").addEventListener("click", function () {
+    window.location.href = "login.html"; // інша сторінка
+});
+
+document.getElementById("osobistiy-kabinet").addEventListener("click", function () {
+    window.location.href = "profile.html"; // інша сторінка
+});
+
+document.getElementById("spovishennya").addEventListener("click", function () {
+    window.location.href = "notifications.html"; // інша сторінка
+});
